c7d7aaf43a2534c7e82b6368c60d5a63ddfc93b5
chding12 <chding12@gmail.com>
2018-11-11 22:15:57 -0500

    updated gen_labels function

--- modules/preprocessing/generate_labels.py
+++ modules/preprocessing/generate_labels.py
@@ -1,45 +1,35 @@
-
-# coding: utf-8
-
-# In[214]:
-
 import pandas as pd
 import numpy as np
 import itertools
 
-def gen_labels(df1_id, df2_id, match_df):
+def gen_labels(df1_id, df2_id, match_df, match_id1, match_id2):
     """
-    Generate labels of the 
+    Generate y labels for the final cross product data
     
-    *** PLEASE READ CAREFULLY THE DESCRIPTION BELOW    ***
     *** PAY CLOSE ATTENTION TO THE ORDER OF THE INPUTS ***
     
     :param df1_id: the id column of the first data frame
     :param df2_id: the id column of the second data frame
     **Order of data frames should be aligned with the similarity functions**
+    
     :param match_df: a data frame containing the pairs of ids that are matches 
-                     The first column should correspond to ids from df1
-                     Second column should correspond to ids from df2
+    :param match_id1: column name in match_df corresponding to df1_id
+    :param match_id2: column name in match_df corresponding to df2_id
+    
     :return y: Labels of the cross product
     
     """ 
 
     cross = []
     # Concatenate each matched id pair into a string
-    match = list(match_df.iloc[:,0].astype('str') +'_'+ match_df.iloc[:,1].astype('str'))
+    match = match_df[match_id1].astype(str) +'_'+ match_df[match_id2].astype(str)
     
     # Get all possible combinations of id pairs
     # Concatenate each pair into a string
     for (id_1,id_2) in itertools.product(list(df1_id), list(df2_id)):
         cross.append(str(id_1) +'_'+ str(id_2))
     
-    # Combine matched pairs with all other pairs, and generate labels
-    labels = np.array([item in match for item in cross]).astype('int')
+    # Generate labels
+    labels = np.array(pd.Series(cross).isin(match).astype(int))
     
     return labels
-
-
-# In[ ]:
-
-
-
--- unit_tests/test_generate_labels.py
+++ unit_tests/test_generate_labels.py
@@ -1,8 +1,3 @@
-
-# coding: utf-8
-
-# In[4]:
-
 import sys
 import pytest
 import numpy as np
@@ -14,16 +9,10 @@ from modules.preprocessing.generate_labels import gen_labels
 def test_gen_labels():
     df1_id = pd.Series([1,2])
     df2_id = pd.Series(['a','b'])
-    match = pd.DataFrame(['a','b'],[1,2]).reset_index()
-    match.columns=(['id1','id2'])
+    match = pd.DataFrame([1,2],['a','b']).reset_index()
+    match.columns=(['id2','id1'])
     
-    tmp = gen_labels(df1_id, df2_id, match)
+    tmp = gen_labels(df1_id, df2_id, match, 'id1','id2')
     desired = np.array([1,0,0,1])
-    
+
     assert(np.array_equal(tmp,desired))
-
-
-# In[ ]:
-
-
-
